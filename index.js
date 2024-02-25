61976458c9c3ae3c0015a649703aa2a5447a9e0a
Gagan-Sky <YOUR_EMAIL>
2024-02-25 10:22:31 +0530

    added controller and changed code in index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+import ProductController from './src/controllers/product.controller';
 
 const server = express();
 
-server.get('/', (req, res) => {
-    return res.send("welcome to Inventory App");
-})
+// calling a method belongs to class
+// create a instance fot the class / ProductController
+
+const productController = new ProductController();
+
+server.get('/', productController.getProducts)
+
+server.use(express.static('src/views'))
 
 server.listen(3400);
