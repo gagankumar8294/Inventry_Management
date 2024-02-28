ce91c706077ac0ded82a0cc8471b4925e28cde79
Gagan-Sky <YOUR_EMAIL>
2024-02-28 10:35:30 +0530

    changed product.html to product.ejs(changed the hardcoded data with .ejs literals to process dynamic data by implementing foreach loop) | updated model |  updated controller (sending res.render with name of the templete & data in key value pair from the getproduct function inside PRODUCCONTROLLER class)

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,8 +4,13 @@ import ProductModel from '../models/product.model.js';
 export default class ProductController {
     getProducts(req, res){
         let products = ProductModel.get();
-        console.log(products);
-        return res.sendFile(path.join(path.resolve(), 'src','views','products.html'));
+        // console.log(products);
+        // return res.sendFile(path.join(path.resolve(), 'src','views','products.html'));
+        
+        // have to specify name of the templete
+        // render function accrpts name of the templete[products] 
+        // & data in key value pair
+        res.render("products", {products:products})
     }
 }
 
