79e8671f69acf28a2941670a49c3295fe914355d
Gagan-Sky <YOUR_EMAIL>
2024-02-28 07:46:50 +0530

    added MODEL - inside created a model of data and imported MODEL in controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,7 +1,10 @@
 import path from 'path';
+import ProductModel from '../models/product.model.js';
 
 export default class ProductController {
     getProducts(req, res){
+        let products = ProductModel.get();
+        console.log(products);
         return res.sendFile(path.join(path.resolve(), 'src','views','products.html'));
     }
 }
