65441cbf2785aa7f6355c20f75d2d353f34e9937
Gagan-Sky <YOUR_EMAIL>
2024-03-17 08:10:22 +0530

    added all the code required for cookies

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ import UserController from './src/controllers/user.controller.js';
 import { uploadFile } from './src/middlewares/file-upload.middleware.js';
 import session from 'express-session';
 import { auth } from './src/middlewares/auth.middleware.js';
+import cookieParser from 'cookie-parser';
+import { setLastVisit } from './src/middlewares/lastVisit.middleware.js';
 
 const server = express();
 
+server.use(cookieParser());
+server.use(setLastVisit);
 
 //sessions
 server.use(
